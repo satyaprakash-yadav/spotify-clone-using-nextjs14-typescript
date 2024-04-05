575eaaa64b707e28c56c85e1a573da735f613060
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-04-05 22:32:09 +0530

    Favorites functionality done

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,31 +1,42 @@
 "use client";
 
 import { Song } from "@/types";
+import { useUser } from "@/hooks/useUser";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
 
-interface SearchContentProps {
+interface LikedContentProps {
   songs: Song[];
 }
 
-const SearchContent = ({ songs }: SearchContentProps) => {
+const LikedContent = ({ songs }: LikedContentProps) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
   if (songs.length === 0) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-        No songs found.
+        No liked songs.
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col gap-y-2 w-full px-6">
+    <div className="flex flex-col gap-y-2 w-full p-6">
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
             <MediaItem onClick={() => {}} data={song} />
           </div>
-          {/* TODO: Add Like Button Here */}
           <LikeButton songId={song.id} />
         </div>
       ))}
@@ -33,4 +44,4 @@ const SearchContent = ({ songs }: SearchContentProps) => {
   );
 };
 
-export default SearchContent;
+export default LikedContent;
